008c864ea1a6ca272b8a881188c24ff4aebf95f7
cartohca <cartohca@mail.ru>
2023-03-28 16:59:12 +0300

    functions

--- lernJs/java.js
+++ lernJs/java.js
@@ -1,3 +1,9 @@
-
-const b="1000000000000000000000087"
-console.log(b)
+function sum(...all){
+    let result=0
+    for(let num of all){
+       result+=num
+    }
+    return result
+}
+const res=sum(20,4,5)
+console.log(res)
