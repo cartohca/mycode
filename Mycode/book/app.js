c3569cc2dbc7878c1cbb2263855dd59c41107cad
cartohca <cartohca@mail.ru>
2023-06-26 17:25:26 +0300

    42

--- Mycode/book/app.js
+++
@@ -1,8 +0,0 @@
-const a = document.querySelector('input')
-const b = document.querySelector('button')
-const c = document.querySelector('p')
-
-
-b.onclick = (el) => {localStorage.setItem('text',a.value)
-c.innerText = a.value}
-console.log(localStorage.getItem('text'))
